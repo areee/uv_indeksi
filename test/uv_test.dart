c4627a2b2934d3fd926de27461e26c48db455a87
areee <10089872+areee@users.noreply.github.com>
2023-04-30 17:03:03 +0300

    Group 2 tests in uv_test

--- test/uv_test.dart
+++ test/uv_test.dart
@@ -2,11 +2,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:uv_indeksi/uv_service.dart';
 
 void main() {
-  test('Read UV data from API', () async {
-    var uv = await fetchUV();
+  group('When fetching UV from API', () {
+    test('Data is not null', () async {
+      var uv = await fetchUV();
 
-    expect(uv, isNotNull);
-    expect(uv.time, isNotNull);
-    expect(uv.value, isNotNull);
+      expect(uv, isNotNull);
+      expect(uv.time, isNotNull);
+      expect(uv.value, isNotNull);
+    });
+
+    test('Data is valid', () async {
+      var uv = await fetchUV();
+
+      expect(uv.time, isA<DateTime>());
+      expect(uv.value, isA<double>());
+    });
   });
 }
